7fcf1ff683a5073d1d28deb0ec86260d13b38bdb
Girish Vadlamudi <girishvadlamudi@outlook.com>
2025-04-15 11:01:11 +0200

    Implement footer navbar with three buttons

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,15 @@
+import Navbar from "@/components/Navbar/Navbar";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer className="bg-gray-900 fixed bottom-0 left-0 right-0">
+        <Navbar />
+      </footer>
+    </>
+  );
 }
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,3 @@
-import Navbar from "@/components/Navbar/Navbar";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -9,7 +8,6 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-      <Navbar />
     </Html>
   );
 }
