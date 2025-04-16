fcff7fd31b91d5c89e1724861dcc3d32fe24f380
Girish Vadlamudi <girishvadlamudi@outlook.com>
2025-04-16 10:15:17 +0200

    Implement favorite feature for each card and also favorites page

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,22 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
+import FavoriteButton from "../FavoriteButton/FavoriteButton";
 
 export default function Card({ element, elementName }) {
   return (
     <Link href={`/${elementName}/${element.slug}`} passHref legacyBehavior>
       <a className="group">
-        <div className="w-full overflow-hidden rounded-lg">
+        <div className="w-full overflow-hidden rounded-lg relative">
           <Image
             width={element.dimensions.width}
             height={element.dimensions.height}
             alt="Artwork image"
             src={
-              element.imageSource
-                ? element.imageSource
-                : "https://images.unsplash.com/photo-1574267432553-4b4628081c31?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"
+              element.imageSource ||
+              "https://images.unsplash.com/photo-1574267432553-4b4628081c31?ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"
             }
             className="w-full h-auto object-cover group-hover:opacity-75"
           />
+          {/* Position the favorite button in the top-right corner */}
+          <div className="absolute top-2 right-2">
+            <FavoriteButton artPiece={element} />
+          </div>
         </div>
         <h3 className="mt-4 text-lg text-gray-700">{element.name}</h3>
         <p className="mt-1 text-sm font-medium text-gray-900">
